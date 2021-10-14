35e3cdf8c6baa97e155b50560a08bb0c2d815109
unknown <altamashbagwan1@gmail.com>
2021-10-14 19:51:17 +0530

    Added: Functioning GenericsProblem Compare Float number

---
+++ com/bl/genericsProblemTestCase/StringToCompareMethod.java
@@ -0,0 +1,22 @@
+package com.bl.genericsProblemTestCase;
+
+public class StringToCompareMethod {
+
+	public static String maximumString(String a, String b, String c) {
+		String maximum = a;
+
+		if (b.compareTo(maximum) > 0) {
+			maximum = b;
+		}
+		if (c.compareTo(maximum) > 0) {
+			maximum = c;
+		}
+		return maximum;
+	}
+
+	public static void main(String[] args) {
+		String maximumName = StringToCompareMethod.maximumString("Apple", "Peach", "Onion");
+		System.out.println("The Greater Value is: " + maximumName);
+	}
+
+}
